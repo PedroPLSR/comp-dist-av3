34ab1cc12803de6ea751bd217b1f5c8a2948d4ac
Alejandro Elias <80522868+ALEliasLM@users.noreply.github.com>
2024-06-10 00:11:45 -0300

    Funções de Create Updade e Delete
    Falta colocar isso em rotas e testar com o postman

--- src/main/java/com/pedror/dados/Dados.java
+++ src/main/java/com/pedror/dados/Dados.java
@@ -25,18 +25,20 @@ public class Dados {
       usuario.setNome(faker.name().fullName());
       usuario.setIdade(faker.number().numberBetween(18, 60));
       usuario.setPlaylists(new ArrayList<>());
+
+      //Usuário criado --------------------
       for (int j = 0; j < 5; j++) {
         Playlist playlist = new Playlist();
-        playlist.setId((long) j);
+        playlist.setId((long) (i*10 + j));
         playlist.setNome(faker.music().genre());
         playlist.setMusicas(new ArrayList<>());
-        for (int k = 0; k < 10; k++) {
+        for (int k = 0; k < 10; k++) { // Cria 10 musicas novas e coloca em uma playlist
           Musica musica = new Musica();
-          musica.setId((long) k);
+          musica.setId((long) (i*10 + j*5 + k));
           musica.setNome(faker.music().instrument());
           musica.setArtista(faker.artist().name());
-          playlist.getMusicas().add(musica);
-          musicas.add(musica);
+          playlist.getMusicas().add(musica); //adiciona a musica criada na playlist atual
+          musicas.add(musica); //Adiciona a musica para a lista global de musicas
         }
         usuario.getPlaylists().add(playlist);
         playlists.add(playlist);
@@ -85,5 +87,93 @@ public class Dados {
     }
 
     return playlistsComMusica;
+  }
+
+
+  //Codigo para o CRUD
+  //Create
+  public void createMusica(String nome, String artista){
+    Musica musica = new Musica();
+    musica.setNome(nome);
+    musica.setArtista(artista);
+    musica.setId(getMusicas().getLast().getId() + 1); //setando pra ser o ultimo + 1
+    getMusicas().add(musica);
+  }
+  public void createUsuario(String nome, int idade, List<Playlist> playlists){
+    Usuario usuario = new Usuario();
+    usuario.setNome(nome);
+    usuario.setIdade(idade);
+    usuario.setPlaylists(playlists);
+    usuario.setId(getUsuarios().getLast().getId() + 1); //setando pra ser o ultimo + 1
+    getUsuarios().add(usuario);
+  }
+
+  public void createPlaylist(String nome, int... idMusicas){ //int... aceita um conjunto de ids pra colocar na playlist
+    Playlist playlist = new Playlist();
+    playlist.setNome(nome);
+    List<Musica> list = new ArrayList<>();
+    for (int id : idMusicas) {
+      list.add(getMusicas().get(id));
+    }
+    playlist.setMusicas(list);
+    playlist.setId(getPlaylists().getLast().getId() + 1);
+    getPlaylists().add(playlist);
+  }
+
+  //Read depende de cada api..
+
+  //Update
+  public void updateMusica(long idMusica, Musica newMusica){
+    for(Musica musica : getMusicas()){
+      if(musica.getId().equals(idMusica)){
+        getMusicas().set(getMusicas().indexOf(musica), newMusica);
+        break;
+      }
+    }
+  }
+
+  public void updateUsuario(long idUsuario, Usuario newUsuario){
+    for(Usuario usuario : getUsuarios()){
+      if(usuario.getId().equals(idUsuario)){
+        getUsuarios().set(getUsuarios().indexOf(usuario), newUsuario);
+        break;
+      }
+    }
+  }
+
+  public void updatePlaylist(long idPlaylist, Playlist newPlaylist){
+    for(Playlist playlist : getPlaylists()){
+      if(playlist.getId().equals(idPlaylist)){
+        getPlaylists().set(getPlaylists().indexOf(playlist), newPlaylist);
+        break;
+      }
+    }
+  }
+
+  //Remove
+
+  public void removeMusic(long idMusica){
+    for(Musica musica : getMusicas()){
+      if(musica.getId().equals(idMusica)){
+        getMusicas().remove(getMusicas().indexOf(musica));
+        break;
+      }
+    }
+  }
+  public void removeUsuario(long idUsuario){
+    for(Usuario usuario : getUsuarios()){
+      if(usuario.getId().equals(idUsuario)){
+        getUsuarios().remove(getMusicas().indexOf(usuario));
+        break;
+      }
+    }
+  }
+  public void removePlaylist(long idPlaylist){
+    for(Playlist playlist : getPlaylists()){
+      if(playlist.getId().equals(idPlaylist)){
+        getPlaylists().remove(getMusicas().indexOf(playlist));
+        break;
+      }
+    }
   }
 }
