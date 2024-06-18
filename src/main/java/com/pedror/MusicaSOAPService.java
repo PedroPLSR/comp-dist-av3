fab8a235e6fcc6af47d4247c5d16212bf2c6812b
ALEliasLM <alejandroelias.lm@gmail.com>
2024-06-18 09:46:16 -0300

    Soap CRUD completo
    sabao cra cra... sabao cra cra...

--- src/main/java/com/pedror/MusicaSOAPService.java
+++ src/main/java/com/pedror/MusicaSOAPService.java
@@ -1,9 +1,6 @@
 package com.pedror;
 
-import com.pedror.dados.Dados;
-import com.pedror.dados.Musica;
-import com.pedror.dados.Playlist;
-import com.pedror.dados.Usuario;
+import com.pedror.dados.*;
 import com.pedror.soap.MusicaSOAPServiceInt;
 import jakarta.inject.Inject;
 import jakarta.jws.WebMethod;
@@ -13,10 +10,29 @@ import java.util.List;
 
 @WebService(name = "MusicaSOAPService", serviceName = "MusicaSOAPService", targetNamespace = "http://musicasoap.com")
 public class MusicaSOAPService implements MusicaSOAPServiceInt {
-
   @Inject
   Dados dados;
 
+  // Create ------------------------------------------------------------------------------------------------------------
+  @WebMethod
+  @Override
+  public Usuario criarUsuario(JSON_Objects.JSON_Usuario usuario) {
+    return dados.createUsuario(usuario);
+  }
+
+  @WebMethod
+  @Override
+  public Musica criarMusica(JSON_Objects.JSON_Musica musica) {
+    return dados.createMusica(musica);
+  }
+
+  @WebMethod
+  @Override
+  public Playlist criarPlaylist(JSON_Objects.JSON_Playlist playlist) {
+    return dados.createPlaylist(playlist);
+  }
+
+  // Read --------------------------------------------------------------------------------------------------------------
   @WebMethod
   @Override
   public List<Usuario> getUsuarios() {
@@ -47,4 +63,41 @@ public class MusicaSOAPService implements MusicaSOAPServiceInt {
     return dados.getPlaylistsComMusica(idMusica);
   }
 
+  // Update ------------------------------------------------------------------------------------------------------------
+  @WebMethod
+  @Override
+  public void updateCliente(Long idUsuario, JSON_Objects.JSON_Usuario usuario) {
+    dados.updateUsuario(idUsuario, dados.createUsuario(usuario));
+  }
+
+  @WebMethod
+  @Override
+  public void updateMusica(Long idMusica, JSON_Objects.JSON_Musica musica) {
+    dados.updateMusica(idMusica, dados.createMusica(musica));
+  }
+
+  @WebMethod
+  @Override
+  public void updatePlaylist(Long idPlaylist, JSON_Objects.JSON_Playlist playlist) {
+    dados.updatePlaylist(idPlaylist, dados.createPlaylist(playlist));
+  }
+
+  // Delete ------------------------------------------------------------------------------------------------------------
+  @WebMethod
+  @Override
+  public String deletarCliente(Long idUsuario) {
+    return dados.removeUsuario(idUsuario);
+  }
+
+  @WebMethod
+  @Override
+  public String deletarMusica(Long idMusica) {
+    return dados.removeMusica(idMusica);
+  }
+
+  @WebMethod
+  @Override
+  public String deletarPlaylist(Long idPlaylist) {
+    return dados.removePlaylist(idPlaylist);
+  }
 }
--- src/main/java/com/pedror/soap/MusicaSOAPServiceInt.java
+++ src/main/java/com/pedror/soap/MusicaSOAPServiceInt.java
@@ -1,5 +1,6 @@
 package com.pedror.soap;
 
+import com.pedror.dados.JSON_Objects;
 import com.pedror.dados.Musica;
 import com.pedror.dados.Playlist;
 import com.pedror.dados.Usuario;
@@ -11,6 +12,17 @@ import java.util.List;
 @WebService(name = "MusicaSOAPService", serviceName = "MusicaSOAPService", targetNamespace = "http://musicasoap.com")
 public interface MusicaSOAPServiceInt {
 
+  // Create
+  @WebMethod
+  Usuario criarUsuario(JSON_Objects.JSON_Usuario usuario);
+
+  @WebMethod
+  Musica criarMusica(JSON_Objects.JSON_Musica musica);
+
+  @WebMethod
+  Playlist criarPlaylist(JSON_Objects.JSON_Playlist playlist);
+
+  // Read
   @WebMethod
   List<Usuario> getUsuarios();
 
@@ -26,4 +38,23 @@ public interface MusicaSOAPServiceInt {
   @WebMethod
   List<Playlist> getPlaylistsComMusica(Long idMusica);
 
+  // Update
+  @WebMethod
+  void updateCliente(Long idUsuario, JSON_Objects.JSON_Usuario usuario);
+
+  @WebMethod
+  void updateMusica(Long idMusica, JSON_Objects.JSON_Musica musica);
+
+  @WebMethod
+  void updatePlaylist(Long idPlaylist, JSON_Objects.JSON_Playlist playlist);
+
+  // Delete
+  @WebMethod
+  String deletarCliente(Long idUsuario);
+
+  @WebMethod
+  String deletarMusica(Long idMusica);
+
+  @WebMethod
+  String deletarPlaylist(Long idPlaylist);
 }
